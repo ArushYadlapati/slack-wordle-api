138bf8b2f22a09aa47e9589b0e98eba665c096c3
ArushYadlapati <arush.yadlapati@gmail.com>
2025-07-08 18:02:36 -0700

    Add word check logic + more api path + helper functions

--- api/index.ts
+++ api/index.ts
@@ -35,6 +35,32 @@ async function getWord() {
     return { data, date };
 }
 
+async function getValidWords() {
+    const res = await fetch (
+        `https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt`
+    );
+
+    if (!res.ok) {
+        throw new Error(`Failed to fetch words list: ${ res.status } ${ res.statusText }`);
+    }
+
+    return (await res.text())
+        .split("\n")
+        .map((word) => word.trim())
+        .filter((word) => word.trim() !== "")
+        .map(word => word.trim().toLowerCase());
+}
+
+function error500Message(c: Context, error: Error) {
+    let errorMessage = "Internal Server Error";
+
+    errorMessage += " " + error.message;
+
+    return c.json({
+        error: "Internal Server Error",
+        details: errorMessage
+    }, 500);
+}
 
 app.get("/wordle/:field?", async (c: Context) => {
     try {
@@ -57,21 +83,88 @@ app.get("/wordle/:field?", async (c: Context) => {
 
         return c.json({ [field]: allowedFields[field as keyof typeof allowedFields] });
     } catch (error) {
-        let errorMessage = "Internal Server Error";
-
-        if (error instanceof Error) {
-            errorMessage += " " + error.message;
-        }
-
-        return c.json({
-            error: "Internal Server Error",
-            details: errorMessage
-        }, 500);
+        error500Message(c, error as Error);
     }
 });
 
+app.get("/game", async (c: Context) => {
+    const baseUrl = new URL(c.req.url).origin;
+    const basePath = "/api/game";
 
+    return c.json({});
+})
 
+app.get("/game/valid", async (c: Context) => {
+    try {
+        let word = c.req.query("word");
+
+        if (!word) {
+            return c.json({error: "Missing parameter"}, 400);
+        }
+
+        word = word.toLowerCase().trim();
+        
+    } catch (error) {
+        error500Message(c, error as Error);
+    }
+})
+
+app.get("/game/check", async (c: Context) => {
+    try {
+        let guess = c.req.query("guess");
+
+        if (!guess) {
+            return c.json({error: "Missing parameter"}, 400);
+        }
+
+        guess = guess.toLowerCase().trim();
+
+        if (guess.length !== 5) {
+            return c.json({}, 400);
+        }
+
+        if (!/^[a-z]+$/.test(guess)) {
+            return c.json({}, 400);
+        }
+
+        const solution = (await getWord()).data.solution.toLowerCase();
+
+        const result: number[] = [];
+        const sL = solution.split("")
+        const gL = guess.split("");
+        const sY = new Array(5).fill(false);
+
+        for (let i = 0; i < 5; i++) {
+            if (gL[i] === sL[i]) {
+                result[i] = 0;
+                sY[i] = true;
+            } else {
+                result[i] = 2;
+            }
+        }
+
+        for (let i = 0; i < 5; i++) {
+            if (result[i] === 2) {
+                for (let j = 0; j < 5; j++) {
+                    if (!sY[j] && gL[i] === sL[j]) {
+                        result[i] = 1;
+                        sY[j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return c.json({
+            guess,
+            solution,
+            result
+        })
+
+    } catch (error) {
+        error500Message(c, error as Error);
+    }
+})
 
 const handler = handle(app);
 
